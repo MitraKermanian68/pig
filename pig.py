c169a888afb76f73dac1700aa09ed2f40e1d5dd3
Mitra Kermani <mitrakermanian68@gmail.com>
2024-03-01 16:50:01 +0100

    first commit

---
+++ pig.py
@@ -0,0 +1,50 @@
+import random
+def roll():
+    min_value =1
+    max_value = 6
+    roll = random.randint(min_value, max_value)
+
+    return roll
+
+while True:
+    players = input("Enter the number of players (2-4): ")
+    if players.isdigit():
+        players = int(players)
+        if 2 <= players <= 4:
+            break
+        else:
+            print("Must be between 2-4 players.")
+    else:
+        print("Invalid, try again.")
+
+max_score = 50
+player_scores = [0 for _ in range(players)]#[0]*players # list of players
+
+while max(player_scores) < max_score:
+    for player_idx in range(players):
+        print("\nPlayer number", player_idx + 1, "turn has just begun!")
+        print("Your total score is:", player_scores[player_idx], "\n")
+        current_score = 0
+
+        while True:
+            should_roll = input("Would you like to roll (y/n)? ")
+            if should_roll.lower() != 'y':
+                break
+            else:
+                value = roll()
+                if value == 1:
+                    print("You rolled a 1.  Your turn is over.")
+                    current_score = 0
+                    break
+                else:
+                    current_score += value
+                    print(f"You rolled a {value}.")
+
+                print(f"Your current score is {current_score}.")
+            
+            player_scores[player_idx] += current_score
+            print(f"Your total score is {player_scores[player_idx]}.")
+
+max_score = max(player_scores)
+winning_idx = player_scores.index(max_score)
+print("Player number", winning_idx + 1, "wins with", max_score, "points.")
